e0daa8e0982bcab5a7b4230dbf9e066e33d5f6f2
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-12-04 08:06:57 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Card';
+import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React - Redux (Simples)</h1>
+      <div className="linha">
+        <Intervalo></Intervalo>
+      </div>
+      <div className="linha">
+        <Card title="Card 2" green>X</Card>
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>Y</Card>
+      </div>
     </div>
   );
 }
