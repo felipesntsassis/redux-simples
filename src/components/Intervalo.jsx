df617595a5eb6c60233b0d2e3970fd5285762670
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2021-01-02 12:16:04 -0300

    Implementação dos action creators e reducers para modificação dos campos máximo ou mínimos.

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,29 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Card from './Card';
 import './Intervalo.css';
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numeros';
 
-export default function Intervalo(props) {
+function Intervalo(props) {
+    const { min, max } = props;
+
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalo">
-                <span>
+                 <span>
                     <strong>Mínimo:</strong>
                     <input
                         type="number"
-                        value={0}
-                        readOnly
+                        value={min}
+                        onChange={e => props.alterarMinimo(+e.target.value)}
                     />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
                     <input 
                         type="number"
-                        value={10}
-                        readOnly
+                        value={max}
+                        onChange={e => props.alterarMaximo(+e.target.value)}
                     />
                 </span>
             </div>
         </Card>
     );
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        alterarMinimo(novoNumero) {
+            // action creator -> action
+            const action = alterarNumeroMinimo(novoNumero);
+            dispatch(action);
+        },
+        alterarMaximo(novoNumero) {
+            const action = alterarNumeroMaximo(novoNumero);
+            dispatch(action);
+        }
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Intervalo);
