9c90cfd60e2df401f9be3de4f1f309631c46b43f
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2021-01-02 10:46:13 -0300

    Remoção das implementações sem redux

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -4,25 +4,23 @@ import Card from './Card';
 import './Intervalo.css';
 
 export default function Intervalo(props) {
-    const {min, max} = props;
-
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <strong>Mínimo:</strong>
-                    <input 
+                    <input
                         type="number"
-                        value={min}
-                        onChange={e => props.onMinChanged(+e.target.value)}
+                        value={0}
+                        readOnly
                     />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
                     <input 
                         type="number"
-                        value={max}
-                        onChange={e => props.onMaxChanged(+e.target.value)}
+                        value={10}
+                        readOnly
                     />
                 </span>
             </div>
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 import Card from './Card';
 
 export default function Sorteio(props) {
-    const { min, max } = props;
-    const sorted = parseInt(Math.random() * (max - min)) + min;
-
     return (
         <Card title="Sorteio de um Número" purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{sorted}</strong>
+                    <strong>10</strong>
                 </span>
             </div>
         </Card>
