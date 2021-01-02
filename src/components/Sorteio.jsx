27fddedd311a401d63128e252324191901ed0443
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2021-01-02 11:29:17 -0300

    Implementação do controle de estado do redux nos componentes Média, Soma e Sorteio.

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Card from './Card';
 
-export default function Sorteio(props) {
+ function Sorteio(props) {
+    const { min, max } = props;
+    const sorted = parseInt(Math.random() * (max - min)) + min;
+
     return (
         <Card title="Sorteio de um Número" purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>10</strong>
+                    <strong>{sorted}</strong>
                 </span>
             </div>
         </Card>
     );
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    };
+}
+
+export default connect(mapStateToProps)(Sorteio);
